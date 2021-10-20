159ce6c5ca92c3c29bd73ac8a9dfc45a40643d0b
Dalibor Marinkovic <dalibor.marinkovic@gmail.com>
2021-10-20 18:35:15 +0200

    create migrations

---
+++ database/migrations/2021_10_20_162958_create_mentors_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMentorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mentors', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('surname');
+            $table->string('city');
+            $table->string('skype');
+            $table->string('username');
+            $table->string('password');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mentors');
+    }
+}
